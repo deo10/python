e3b5170d08b4cd7e3e9883376642c192e675641c
deo10 <pipazoglov@gmail.com>
2024-09-08 23:16:38 +0600

    adding chapter 4 about mutable\immutable objects

---
+++ chapter_4.py
@@ -0,0 +1,19 @@
+# objects
+
+# imutable objects - if new value is assign then new object will be created
+
+str
+int
+bool
+float
+tuple
+range
+None
+
+# mutable objects - if new value would be assign then existing object will be changed
+
+list
+dict
+set
+#user-defined-classes
+
