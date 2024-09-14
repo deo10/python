b3023492a4cb0ee7da0117ba913a3cbef2aef06e
deo10 <pipazoglov@gmail.com>
2024-09-14 23:31:32 +0600

    adding part for str, int, bool, float - change and converation

--- chapter_5.py
+++ chapter_5.py
@@ -55,3 +55,30 @@ print(sum)
 
 print(type(sum))
 # complex
+
+
+
+# bool
+
+is_authorize = True # bool - true or false
+print(is_authorize)
+print(type(is_authorize))  # bool
+
+print(5 > 3)  # True
+
+print(5 < 3)  # False
+
+print('Long string' > 'Long')  # True
+print('long string' > 'Short')  # False ! посимвольное сравнение
+print('Long string' > 'long')  # False ! 
+
+print([1, 2, 3] == [1, 2, 3])  # True
+print({'a': 3} == {'a': 3})  # True
+print({'a': 3} == {'a': 5})  # False
+
+print(bool(10)) # True
+print(bool('abc')) # True
+print(bool([]))  # False !
+print(bool([1, 2]))  # True
+print(bool(None))  # False !
+
---
+++ chapter_6.py
@@ -0,0 +1,28 @@
+# конвертация типов
+
+# явная конвертация типов
+
+str
+int
+float
+tuple
+list
+set
+
+'10' + 5  # error str cannot add int
+
+5 + int('10') # works явная конвертация
+
+# неявная конвертация 
+
+int_num = 5
+float_num = 5.5
+
+print(int_num + float_num)  # works 10.5
+
+bool_val = True
+int_val = 7
+
+print(bool_val + int_val) # works 8
+
+
