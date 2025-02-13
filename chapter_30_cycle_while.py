f5f1fb59e2c8aec5d38272da9ca9d1a68e1d1cf5
deo10 <pipazoglov@gmail.com>
2025-02-13 21:09:23 +0600

    more examples

--- chapter_30_cycle_while.py
+++ chapter_30_cycle_while.py
@@ -35,3 +35,29 @@ while True:
         continue # back to num input
     print("Congrats!", random_num)
     break
+
+#another example
+i = 5
+while True:
+    if i%0o11 == 0: #first comment below
+        break
+    print(i)
+    i += 1
+
+# This line checks if the value of `i` is divisible by `0o11`
+# (octal representation of the decimal number `9`).
+# The `%` operator is the modulus operator, which returns the remainder of the division.
+# If `i` is divisible by `9`, the condition will be `True`.
+
+### Summary of the Loop Execution:
+# - The loop starts with `i` initialized to `5`.
+# - It checks if `i` is divisible by `9` (since `0o11` in octal is `9` in decimal).
+# - If `i` is not divisible by `9`, it prints the value of `i` and increments `i` by `1`.
+# - The loop continues until `i` becomes `9`, at which point the condition `i % 0o11 == 0` becomes `True`, and the loop breaks.
+
+### Output:
+# The output of the code will be:
+# 5
+# 6
+# 7
+# 8
--- check_code.py
+++ check_code.py
@@ -1,28 +1,47 @@
-import random
+matrix = [[[0, 1, 2], [0, 1, 2], [0, 1, 2]], [[0, 1, 2], [0, 1, 2], [0, 1, 2]], [[0, 1, 2], [0, 1, 2], [0, 1, 2]]]
 
-def is_user_auth():
-    return random.choice([True, False]) 
+matrix2 = []
+
+for submatrix in matrix:
+  for val in submatrix:
+    matrix2.append(val)
+
+print(matrix2)
+print(matrix2[2])
+print(matrix2[2][0])
 
 
-def check_user_auth(fn):
-    def wrapper(*args, **kwargs):
-        if is_user_auth(): # if True
-            print("User is authenticated")
-            return fn(*args, **kwargs)
-        else: # if False
-            raise Exception("ERROR: User is not authenticated")
-    
-    return wrapper
 
-@check_user_auth
-def do_sensetive_job():
-    # do some tasks if only user authenticated
-    print("Results of the some sensetive tasks")
+i = 5
+while True:
+    if i%0o11 == 0: #first comment below
+        break
+    print(i)
+    i += 1
 
-try:
-    do_sensetive_job()
-    do_sensetive_job()
-    do_sensetive_job()
-    do_sensetive_job()
-except Exception as e:
-    print(e)
+# This line checks if the value of `i` is divisible by `0o11`
+# (octal representation of the decimal number `9`).
+# The `%` operator is the modulus operator, which returns the remainder of the division.
+# If `i` is divisible by `9`, the condition will be `True`.
+
+### Summary of the Loop Execution:
+# - The loop starts with `i` initialized to `5`.
+# - It checks if `i` is divisible by `9` (since `0o11` in octal is `9` in decimal).
+# - If `i` is not divisible by `9`, it prints the value of `i` and increments `i` by `1`.
+# - The loop continues until `i` becomes `9`, at which point the condition `i % 0o11 == 0` becomes `True`, and the loop breaks.
+
+### Output:
+# The output of the code will be:
+# 5
+# 6
+# 7
+# 8
+
+testdict = {
+  "brand": "Samsung",
+  "ram": "3",
+  "Os": "Android",
+  "year": 2020
+}
+
+print(testdict.keys())
